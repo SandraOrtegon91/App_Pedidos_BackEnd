52e69c7656d002487ce33cbf70e0cbef5e1b4e78
Sandra Milena Ortegon <x56-ella@hotmial.com>
2021-11-03 18:05:29 -0500

    versión 1.0.0 de la aplicación de pedidos

---
+++ src/models/pedido.model.ts
@@ -0,0 +1,54 @@
+import {belongsTo, Entity, hasOne, model, property} from '@loopback/repository';
+import {Producto} from '.';
+import {Persona} from './persona.model';
+
+@model()
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  producto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  estado: number;
+
+  @belongsTo(() => Persona)
+  personaId: string;
+
+  @hasOne(()=>Producto)
+  Producto: Producto;
+
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
